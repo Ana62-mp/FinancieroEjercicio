92e95c8b271119944d755e477c3b2f33a8612b78
Ana62-mp <ana.moposita84@gmail.com>
2026-04-20 20:31:11 -0500

    APMP: Creación de clases Cuenta y Cliente, sus constructores, get/set, metodo imprimir(). Creación primer test

---
+++ src/com/krakedev/financiero/tests/TestCuenta.java
@@ -0,0 +1,10 @@
+package com.krakedev.financiero.tests;
+
+import com.krakedev.financiero.entidades.Cuenta;
+
+public class TestCuenta {
+	public static void main(String[] args) {
+		Cuenta cuenta1 = new Cuenta("1475");
+		
+	}
+}
